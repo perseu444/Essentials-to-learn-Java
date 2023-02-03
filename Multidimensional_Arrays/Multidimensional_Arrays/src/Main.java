677e4c027ab65a0e272d79769dd8c60b186ccf8b
Jader Marcelo <71997803+jadermarcelo1@users.noreply.github.com>
2023-02-03 16:38:50 -0300

    Add files via upload

---
+++ Multidimensional_Arrays/Multidimensional_Arrays/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int fallafel[][][] = new int [6][4][10];
+        for(int i = 0; i < 6; i++){
+            for(int j = 0; j < 4; j++){
+                for(int k = 0; k < 10; k++){
+                    System.out.print(fallafel[i][j][k]);
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}
